75ac4b62bc9325383c72cf6e771e76dd48cc4d66
aravilalado <b2029kdvillados@pshs.edu.ph>
2025-08-29 11:54:19 +0800

    4th graded (29 aug)

---
+++ public/index.js
@@ -0,0 +1,34 @@
+
+
+process.stdin.on('data', input => {
+
+  let greeting = input.toString().trim();
+switch (greeting) {
+  case "HELLO":
+    console.log("ENGLISH");
+    break;
+  case "MABUHAY":
+    console.log("TAGALOG");
+    break;
+  case "HOLA":
+    console.log("SPANISH");
+    break;
+  case "HALLO":
+    console.log("GERMAN");
+    break;
+  case "BONJOUR":
+    console.log("FRENCH");
+    break;
+  case "CIAO":
+    console.log("ITALIAN");
+    break;
+  case  "ZDRAVSTUJTE":
+    console.log("RUSSIAN");
+}
+
+  // end of your code
+  // do not remove the lines below
+
+  process.exit();
+
+});
